8b8f0bcc608f1644ac15b789e1b666b2a688f687
阿洋 <rednaxela64@outlook.com>
2022-07-25 16:24:07 +0000

    changed file paths

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require("webpack");
 module.exports = {
   entry: "/src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     libraryTarget: "umd",
     umdNamedDefine: true,
     publicPath: "/",
